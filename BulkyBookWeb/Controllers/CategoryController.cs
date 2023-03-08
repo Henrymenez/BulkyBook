68b472429530ec51a9ab031582d2cd6bc6dd7b72
USER <henryugochukwuagu@gmail.com>
2023-03-08 06:11:48 +0100

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicaionDbContext _db;
+
+        public CategoryController(ApplicaionDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categorylist = _db.categories;
+            return View(categorylist);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Display Order and Name Can not be the same");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ BulkyBookWeb/Data/ApplicaionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicaionDbContext : DbContext
+    {
+        public ApplicaionDbContext(DbContextOptions<ApplicaionDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> categories { get; set; }
+    }
+}
